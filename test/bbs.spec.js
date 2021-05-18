18d4b02158a7028250eb202075319b1e92274249
Vitor Pamplona <vitor@vitorpamplona.com>
2021-05-18 18:30:45 -0400

    Adding EU's Certificates for testing.

--- test/bbs.spec.js
+++ test/bbs.spec.js
@@ -14,7 +14,6 @@ const TEST_PAYLOAD = {
   "type": [
     "VerifiableCredential"
   ],
-  "expirationDate": "2021-02-05T20:29:37Z",
   "credentialSubject": {
     "type": "DGCProofOfCovidTest",
     "testInformation": {
@@ -127,9 +126,6 @@ describe('BBS Data Minimization', function() {
 describe('BBS Soup to Nuts', function() {
   it('should Sign Pack And Unpack Verify JSON', async function() {
     const uri = await signAndPack(TEST_PAYLOAD, mockKeyPair);
-
-    console.log(uri);
-
     const resultJSON = await unpackAndVerify(uri);
 
     expect(resultJSON.proof).to.not.be.null;
@@ -141,7 +137,204 @@ describe('BBS Soup to Nuts', function() {
         return item !== "https://w3id.org/security/suites/ed25519-2020/v1"
     })
     delete resultJSON["issuanceDate"]; // These change when tests run
+    delete resultJSON["expirationDate"]; // These change when tests run
     delete resultJSON["issuer"]; // These change when tests run
     expect(resultJSON).to.eql(TEST_PAYLOAD);
+  });
+});
+
+
+const DGCProofOfRecovery = {
+  "@context": [
+    "https://www.w3.org/2018/credentials/v1",
+    "https://w3id.org/pathogen/v1",
+    "https://w3id.org/security/bbs/v1"
+  ],
+  "type": [
+    "VerifiableCredential"
+  ],
+  "credentialSubject": {
+    "@context": [
+      "https://w3id.org/pathogen/v1"
+    ],
+    "type": "DGCProofOfRecovery",
+    "id": "urn:uvci:01:NL:LSP/REC/1289821",
+    "infectionInformation": {
+      "@context": [
+        "https://w3id.org/pathogen/v1"
+      ],
+      "type": "DGCInfectionInformation",
+      "countryOfTest": "NL",
+      "dateFirstPositive": "2021-04-21",
+      "diseaseRecoveredFrom": "840539006"
+    },
+    "issuerName": "Ministry of VWS",
+    "personalInformation": {
+      "@context": [
+        "https://w3id.org/pathogen/v1"
+      ],
+      "type": "DGCSubject",
+      "birthDate": "2009-02-28",
+      "familyName": "d'ArsÃ¸ns - van Halen",
+      "givenName": "FranÃ§ois-Joan",
+      "stdFamilyName": "DARSONS<VAN<HALEN",
+      "stdGivenName": "FRANCOIS<JOAN"
+    },
+    "validFrom": "2021-05-01",
+    "validUntil": "2021-10-21"
+  },
+  "evidence": [],
+}
+
+const DGCProofOfCovidTest = {
+  "@context": [
+    "https://www.w3.org/2018/credentials/v1",
+    "https://w3id.org/pathogen/v1",
+    "https://w3id.org/security/bbs/v1"
+  ],
+  "type": [
+    "VerifiableCredential"
+  ],
+  "credentialSubject": {
+    "@context": [
+      "https://w3id.org/pathogen/v1"
+    ],
+    "type": "DGCProofOfCovidTest",
+    "id": "urn:uvci:01:NL:GGD/81AAH16AZ",
+    "issuerName": "Ministry of VWS",
+    "personalInformation": {
+      "@context": [
+        "https://w3id.org/pathogen/v1"
+      ],
+      "type": "DGCSubject",
+      "birthDate": "2009-02-28",
+      "familyName": "d'ArsÃ¸ns - van Halen",
+      "givenName": "FranÃ§ois-Joan",
+      "stdFamilyName": "DARSONS<VAN<HALEN",
+      "stdGivenName": "FRANCOIS<JOAN"
+    },
+    "testInformation": {
+      "@context": [
+        "https://w3id.org/pathogen/v1"
+      ],
+      "type": "DGCTestInformation",
+      "countryOfTestAdminstration": "NL",
+      "diseaseTestedFrom": "840539006",
+      "sampleCollectionDateTime": "2021-04-13T14:20:00+00:00",
+      "testCenter": "GGD FryslÃ¢n, L-Heliconweg",
+      "testManufacturer": "1232",
+      "testName": "COVID PCR",
+      "testResult": "260415000",
+      "testResultDate": "2021-04-13T14:40:01+00:00",
+      "testType": "LP217198-3"
+    }
+  },
+  "evidence": [],
+}
+
+const DGCProofOfVaccination = {
+  "@context": [
+    "https://www.w3.org/2018/credentials/v1",
+    "https://w3id.org/pathogen/v1",
+    "https://w3id.org/security/bbs/v1"
+  ],
+  "type": [
+    "VerifiableCredential"
+  ],
+  "credentialSubject": {
+    "@context": [
+      "https://w3id.org/pathogen/v1"
+    ],
+    "type": "DGCProofOfVaccination",
+    "id": "urn:uvci:01:NL:PlA8UWS60Z4RZXVALl6GAZ",
+    "issuerName": "Ministry of VWS",
+    "personalInformation": {
+      "@context": [
+        "https://w3id.org/pathogen/v1"
+      ],
+      "type": "DGCSubject",
+      "birthDate": "2009-02-28",
+      "familyName": "d'ArsÃ¸ns - van Halen",
+      "givenName": "FranÃ§ois-Joan",
+      "stdFamilyName": "DARSONS<VAN<HALEN",
+      "stdGivenName": "FRANCOIS<JOAN"
+    },
+    "vaccinationInformation": {
+      "@context": [
+        "https://w3id.org/pathogen/v1"
+      ],
+      "type": "DGCVaccinationInformation",
+      "countryOfVaccination": "NL",
+      "dateOfVaccination": "2021-05-18",
+      "diseaseProtectedFrom": "840539006",
+      "order": "2 of 2",
+      "prophylaxis": "1119349007",
+      "vaccine": {
+        "@context": [
+          "https://w3id.org/pathogen/v1"
+        ],
+        "type": "DGCVaccine",
+        "code": "1119349007",
+        "marketingAuthHolder": "ORG-100030215",
+        "medicinalProductName": "EU/1/20/1528"
+      }
+    }
+  },
+  "evidence": []
+}
+
+describe('DGC Soup to Nuts', function() {
+  it('should Sign Pack And Unpack Verify DGCProofOfRecovery', async function() {
+    const uri = await signAndPack(DGCProofOfRecovery, mockKeyPair);
+    const resultJSON = await unpackAndVerify(uri);
+
+    expect(resultJSON.proof).to.not.be.null;
+    expect(resultJSON.issuer).to.not.be.null;
+    expect(resultJSON.issuanceDate).to.not.be.null;
+
+    // Removing added elements to match initial payload. 
+    resultJSON["@context"] = resultJSON["@context"].filter(function(item) {
+        return item !== "https://w3id.org/security/suites/ed25519-2020/v1"
+    })
+    delete resultJSON["issuanceDate"]; // These change when tests run
+    delete resultJSON["expirationDate"]; // These change when tests run
+    delete resultJSON["issuer"]; // These change when tests run
+    expect(resultJSON).to.eql(DGCProofOfRecovery);
+  });
+
+  it('should Sign Pack And Unpack Verify DGCProofCOVIDTest', async function() {
+    const uri = await signAndPack(DGCProofOfCovidTest, mockKeyPair);
+    const resultJSON = await unpackAndVerify(uri);
+
+    expect(resultJSON.proof).to.not.be.null;
+    expect(resultJSON.issuer).to.not.be.null;
+    expect(resultJSON.issuanceDate).to.not.be.null;
+
+    // Removing added elements to match initial payload. 
+    resultJSON["@context"] = resultJSON["@context"].filter(function(item) {
+        return item !== "https://w3id.org/security/suites/ed25519-2020/v1"
+    })
+    delete resultJSON["issuanceDate"]; // These change when tests run
+    delete resultJSON["expirationDate"]; // These change when tests run
+    delete resultJSON["issuer"]; // These change when tests run
+    expect(resultJSON).to.eql(DGCProofOfCovidTest);
+  });
+
+  it('should Sign Pack And Unpack Verify DGCProofOfVaccination', async function() {
+    const uri = await signAndPack(DGCProofOfVaccination, mockKeyPair);
+    const resultJSON = await unpackAndVerify(uri);
+
+    expect(resultJSON.proof).to.not.be.null;
+    expect(resultJSON.issuer).to.not.be.null;
+    expect(resultJSON.issuanceDate).to.not.be.null;
+
+    // Removing added elements to match initial payload. 
+    resultJSON["@context"] = resultJSON["@context"].filter(function(item) {
+        return item !== "https://w3id.org/security/suites/ed25519-2020/v1"
+    })
+    delete resultJSON["issuanceDate"]; // These change when tests run
+    delete resultJSON["expirationDate"]; // These change when tests run
+    delete resultJSON["issuer"]; // These change when tests run
+    expect(resultJSON).to.eql(DGCProofOfVaccination);
   });
 });
